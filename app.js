dc549190fe156c65f654f92a279764cd7115ae78
Rodolfo Rios <rodolforios18@gmail.com>
2021-09-20 22:02:52 -0300

    modal

--- app.js
+++ app.js
@@ -1,45 +1,45 @@
 class Despesa {
-    constructor(ano, mes, dia, tipo, descricao, valor) {
-        this.ano = ano
-        this.mes = mes
-        this.dia = dia
-        this.tipo = tipo
-        this.descricao = descricao
-        this.valor = valor
-    }
-    validarDados() {
-        for (let i in this) {
-            if (this[i] == undefined || this[i] == '' || this[i] == null) {
-                return false
-            }
-        }
-        return true 
-    }
-}
+	constructor(ano, mes, dia, tipo, descricao, valor) {
+		this.ano = ano
+		this.mes = mes
+		this.dia = dia
+		this.tipo = tipo
+		this.descricao = descricao
+		this.valor = valor
+	}
 
+	validarDados() {
+		for(let i in this) {
+			if(this[i] == undefined || this[i] == '' || this[i] == null) {
+				return false
+			}
+		}
+		return true
+	}
+}
 
 class Bd {
 
-    constructor() {
-        let id = localStorage.getItem('id')
+	constructor() {
+		let id = localStorage.getItem('id')
 
-        if (id === null) {
-            localStorage.setItem('id', 0)
-        }
-    }
+		if(id === null) {
+			localStorage.setItem('id', 0)
+		}
+	}
 
-    getProximoId() {
-        let proximoId = localStorage.getItem('id')
-        return parseInt(proximoId) + 1
-    }
+	getProximoId() {
+		let proximoId = localStorage.getItem('id')
+		return parseInt(proximoId) + 1
+	}
 
-    gravar(d) {
-        let id = this.getProximoId()
+	gravar(d) {
+		let id = this.getProximoId()
 
-        localStorage.setItem(id, JSON.stringify(d))
+		localStorage.setItem(id, JSON.stringify(d))
 
-        localStorage.setItem('id', id)
-    }
+		localStorage.setItem('id', id)
+	}
 }
 
 let bd = new Bd()
@@ -47,29 +47,28 @@ let bd = new Bd()
 
 function cadastrarDespesa() {
 
-    let ano = document.getElementById('ano')
-    let mes = document.getElementById('mes')
-    let dia = document.getElementById('dia')
-    let tipo = document.getElementById('tipo')
-    let descricao = document.getElementById('descricao')
-    let valor = document.getElementById('valor')
+	let ano = document.getElementById('ano')
+	let mes = document.getElementById('mes')
+	let dia = document.getElementById('dia')
+	let tipo = document.getElementById('tipo')
+	let descricao = document.getElementById('descricao')
+	let valor = document.getElementById('valor')
 
-    let despesa = new Despesa(
-        ano.value,
-        mes.value,
-        dia.value,
-        tipo.value,
-        descricao.value,
-        valor.value
-    )
-    if (despesa.validarDados()) {
-        //bd.gravar(despesa)
-        //dialog de sucesso
-        console.log('Dados válidos')
-
-    } else {
-        //dialog de erro
-        console.log('Dados invalidos')
-    }
+	let despesa = new Despesa(
+		ano.value, 
+		mes.value, 
+		dia.value, 
+		tipo.value, 
+		descricao.value,
+		valor.value
+	)
+	
+	if(despesa.validarDados()) {
+		bd.gravar(despesa)
+		//dialog de sucesso
+		$('#sucessoGravacao').modal('show') 
+	} else {
+		//dialog de sucesso
+		$('#erroGravacao').modal('show') 
+	}
 }
-
